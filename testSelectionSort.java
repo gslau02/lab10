f5b11d35ea6cca65308c8e454b4ca1ab97f96dcd
Ge Siong Lau <gslau@umich.edu>
2022-12-01 15:52:41 +0800

    Tom added test positive and test negative

--- testSelectionSort.java
+++ testSelectionSort.java
@@ -36,12 +36,43 @@ public class testSelectionSort {
         Sortedarr[3] = 9;
         Sortedarr[4] = 10;
 
-        /** add tests to check for this unit test **/
+        SelectionSort temp = new SelectionSort();
+        int[] output = new int[5];
+        output = temp.basicSelectionSort(arr);
+        String Outstr = "";
+        for (int i = 0; i < 5; i++)
+        	Outstr = Outstr + output[i] + " ";
+        String Sortedstr = "";
+        for (int i = 0; i < 5; i++)
+        	Sortedstr = Sortedstr + Sortedarr[i] + " ";
+        assertEquals("Test Positive failed.", Sortedstr , Outstr);
     }
 
-		public void testNegative(){
+    public void testNegative(){
+        int[] arr = new int[5];
+        arr[0] = -8;
+        arr[1] = -9;
+        arr[2] = -7;
+        arr[3] = -10;
+        arr[4] = -2;
 
-        /** Test data contains negative values only **/
+        int[] Sortedarr = new int[5];
+        Sortedarr[0] = -10;
+        Sortedarr[1] = -9;
+        Sortedarr[2] = -8;
+        Sortedarr[3] = -7;
+        Sortedarr[4] = -2;
+
+        SelectionSort temp = new SelectionSort();
+        int[] output = new int[5];
+        output = temp.basicSelectionSort(arr);
+        String Outstr = "";
+        for (int i = 0; i < 5; i++)
+        	Outstr = Outstr + output[i] + " ";
+        String Sortedstr = "";
+        for (int i = 0; i < 5; i++)
+        	Sortedstr = Sortedstr + Sortedarr[i] + " ";
+        assertEquals("Test Negative failed.", Sortedstr , Outstr);
     }
 
 		public void testMixed(){
